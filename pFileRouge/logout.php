049edbddf7befe85de0997fa8d4ad782aa9ff139
AMHIHOSSAMA <92022179+AMHIHOSSAMA@users.noreply.github.com>
2022-06-22 11:41:52 +0100

    Delete pFileRouge directory

--- pFileRouge/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    include('header.php');
-    include('datacon.php');
-
-    unset($_SESSION['id_free']);
-    unset($_SESSION['id_cli']);
-    header("Location:login.php");
-?>
